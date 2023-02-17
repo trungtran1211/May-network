4e56f4bca95de751061ffeca7288c98a90317da6
trungtran1211 <thanhtrungtran8888@gmail.com>
2023-02-17 09:36:09 +0700

    17/2

--- wp-content/themes/my_network/assets/js/main.js
+++ wp-content/themes/my_network/assets/js/main.js
@@ -16,3 +16,32 @@ btnclosemenu.onclick = () => {
 $('main').click(function(){
   listmenu.classList.remove("show-menu");
 });
+
+
+
+
+(function () {
+
+  /**
+   * Main stopscrollwheelzoom constructor
+   */
+  let SSWZ = function () {
+
+      /**
+       * Handler for scroll- control must be pressed.
+       * @param e
+       */
+      this.keyScrollHandler = function (e) {
+          if (e.ctrlKey) {
+              e.preventDefault();
+              return false;
+          }
+      }
+  };
+
+  if (window === top) {
+      let sswz = new SSWZ();
+      window.addEventListener('wheel', sswz.keyScrollHandler, { passive: false });
+  }
+
+})();
